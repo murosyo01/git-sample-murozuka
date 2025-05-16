1f48a576a8aaf035e98abd30eb38acf82e974d9a
murosyo01 <murozuka75@gmail.com>
2025-05-16 16:06:44 +0900

    UserFormにエラーメッセージを追加

--- src/main/java/com/example/controller/Exam04Controller.java
+++ src/main/java/com/example/controller/Exam04Controller.java
@@ -4,9 +4,12 @@ import com.example.domain.User;
 import com.example.form.UserForm;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
+import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 @Controller
 @RequestMapping("/exam04")
@@ -18,19 +21,23 @@ public class Exam04Controller {
     }
 
     @GetMapping("/redirect")
-    public String index2(UserForm userForm){
+    public String index2(UserForm userForm, Model model){
         return "exam04";
     }
 
     @PostMapping("/result")
-    public String result(UserForm userForm, Model model){
+    public String result(@Validated UserForm userForm, BindingResult result, RedirectAttributes redirectAttributes, Model model){
+        if (result.hasErrors()){
+            return index2(userForm,model);
+        }
+
         User user = new User();
 
         user.setName(userForm.getName());
         user.setAge(userForm.getAge());
         user.setComment(userForm.getComment());
 
-        model.addAttribute("user", user);
+        redirectAttributes.addFlashAttribute("user", user);
 
         return "exam04-result";
     }
--- src/main/java/com/example/form/UserForm.java
+++ src/main/java/com/example/form/UserForm.java
@@ -1,8 +1,18 @@
 package com.example.form;
 
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Pattern;
+
 public class UserForm {
+
+    @NotBlank(message = "名前の入力は必須です")
     private String name;
+
+    @NotBlank(message = "年齢の入力は必須です")
+    @Pattern(regexp = "^\\d+$", message = "年齢は数値を入力してください")
     private String age;
+
+    @NotBlank(message = "コメントの入力は必須です。")
     private String comment;
 
     public String getName() {
