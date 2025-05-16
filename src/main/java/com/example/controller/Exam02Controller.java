2c53249cf70cf6007520d7586e983e108da03bcc
murosyo01 <murozuka75@gmail.com>
2025-05-16 14:04:39 +0900

    Exam02Controllerを修正

--- src/main/java/com/example/controller/Exam02Controller.java
+++ src/main/java/com/example/controller/Exam02Controller.java
@@ -1,7 +1,8 @@
 package com.example.controller;
 
+import jakarta.servlet.http.HttpSession;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -9,16 +10,26 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @Controller
 @RequestMapping("/exam02")
 public class Exam02Controller {
+
+    @Autowired
+    private HttpSession session;
+
     @GetMapping("")
     public String index(){
         return "exam02";
     }
 
     @PostMapping("/result")
-    public String result(String num1, String num2, Model model){
-        model.addAttribute("num1", num1);
-        model.addAttribute("num2", num2);
-        model.addAttribute("result", Integer.parseInt(num1) + Integer.parseInt(num2));
+    public String result(String num1, String num2){
+        session.setAttribute("num1", num1);
+        session.setAttribute("num2", num2);
+        session.setAttribute("result", Integer.parseInt(num1) + Integer.parseInt(num2));
+
         return "exam02-result";
+    }
+
+    @GetMapping("/result2")
+    public String result2(){
+        return "exam02-result2";
     }
 }
